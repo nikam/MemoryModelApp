3c95047c4b94d1de20f2102dd3f09e3572d7f1f3
nikam <connect08nikam@gmail.com>
2023-03-26 19:29:46 +0530

    removed comments from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,9 @@ import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 
 import 'package:path_provider/path_provider.dart';
-import 'package:platform/platform.dart';
+//import 'package:platform/platform.dart';
 
 import 'dart:io';
-
-//typedef run_test = Int32 Function(Pointer<Utf8> a, Pointer<Utf8> b,Pointer<Utf8> c,Pointer<Utf8> d,Pointer<Utf8> e);
-//typedef run = Void Function(Pointer<Utf8> a, Pointer<Utf8> b,Pointer<Utf8> c,Pointer<Utf8> d,Pointer<Utf8> e);
 
 class FFIBridge {
   static const platformMethodChannel =
@@ -30,50 +27,15 @@ class FFIBridge {
     return tmp;
   }
 
-  // static Future<String> getTemporaryDirectory() async {
-  //   String tempPath = await getTemporaryDirectory();
-  //   // String tempPath = tempDir.path;
-
-  //   print(tempPath);
-
-  //   return tempPath;
-  // }
-
   static void call() async {
     // print("inside aysnc");
 
     var shaderComp = await printy("assets/litmustest_store_default.spv");
     var shaderRes = await printy("assets/litmustest_store_results.spv");
     var para = await printy("assets/parameters_basic.txt");
-    // var output = await printy("assets/output.txt");
-
-    // var end = para.length - "assets/parameters_basic.txt".length;
-    // var path = para.substring(0, end);
 
     Directory tempDir = await getTemporaryDirectory();
     String path = tempDir.path;
-
-    print("");
-
-    print("output file:");
-
-    print("");
-
-    print(path);
-
-    print("");
-    print("");
-
-    // print(shaderComp);
-    //print(output);
-
-    // DynamicLibrary nativeApiLib = DynamicLibrary.process();
-
-    // final runTest = nativeApiLib
-    //     .lookup<NativeFunction<Int32 Function()>>('runTest')
-    //     .asFunction<int Function()>();
-
-    // runTest();
 
     DynamicLibrary nativeApiLib = DynamicLibrary.process();
     // final runPointer = nativeApiLib.lookup<NativeFunction<run_test>>('runTest');
@@ -90,9 +52,6 @@ class FFIBridge {
     String test = "Store";
     run(test.toNativeUtf8(), shaderComp.toNativeUtf8(),
         shaderRes.toNativeUtf8(), para.toNativeUtf8(), path.toNativeUtf8());
-
-    // path += "output.txt";
-    //print(File(path));
   }
 
   static bool initialize() {
@@ -102,7 +61,6 @@ class FFIBridge {
 }
 
 void main() {
-  print("we are here");
   runApp(const MyApp());
   FFIBridge.initialize();
 }
